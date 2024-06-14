2f15fb1cfe3ee4883a6f8defbb94e0a88f742386
GiaHuyCodeCode <hubby1364@gmail.com>
2024-06-14 09:28:35 +0700

    Update CheckOut

--- app/api/[storeId]/checkout/route.ts
+++ app/api/[storeId]/checkout/route.ts
@@ -2,78 +2,84 @@ import Stripe from "stripe";
 import { NextResponse } from "next/server";
 import { stripe } from "@/lib/stripe";
 import { db } from "@/lib/firebase";
-import {Product} from "@/type-db";
-import {addDoc,collection,doc,serverTimestamp,updateDoc} from "firebase/firestore";
+import { Product } from "@/type-db";
+import {
+  addDoc,
+  collection,
+  doc,
+  serverTimestamp,
+  updateDoc,
+} from "firebase/firestore";
 
 const corsHeaders = {
-    "Access-Control-Allow-Origin" : "*",
-    "Access-Control-Allow-Methods":"GET,POST,PUT,DELETE,OPTIONS",
-    "Access-Control-Allow-Headers":"Content-Type, Authorization",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET,POST,PUT,DELETE,OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
 };
 
 export const OPTIONS = async () => {
-    return NextResponse.json({}, {headers:corsHeaders});
+  return NextResponse.json({}, { headers: corsHeaders });
 };
 
 export const POST = async (
-    req:Request,
-    {params}:{params:{storeId:string}}
-)=>{    
-    const {products, userId}=await req.json();
+  req: Request,
+  { params }: { params: { storeId: string } }
+) => {
+  const { products, userId } = await req.json();
 
-    const line_items : Stripe.Checkout.SessionCreateParams.LineItem[] = [];
+  const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
 
-    products.forEach((item:Product) => {
-        line_items.push({
-            quantity:item.quantity,
-            price_data: {
-                currency:"USD",
-                product_data:{
-                    name:item.name,
-                },
-                unit_amount: Math.round(item.price * 100),
-            },
-        });
+  products.forEach((item: Product) => {
+    line_items.push({
+      quantity: item.quantity ?? (item as typeof item & { qty: number }).qty, // cheating memory ,
+      price_data: {
+        currency: "USD",
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: Math.round(item.price * 100),
+      },
     });
+  });
 
-    
-    const orderData={
-    isPaid:false,
-    orderItems:products,
+  const orderData = {
+    isPaid: false,
+    orderItems: products,
     userId,
-    order_status:"Processing",
+    order_status: "Processing",
     createAt: serverTimestamp(),
-  }; 
+  };
 
   const orderRef = await addDoc(
-    collection(db,"stores",params.storeId,"orders"), orderData
+    collection(db, "stores", params.storeId, "orders"),
+    orderData
   );
 
   const id = orderRef.id;
 
-  await updateDoc(doc(db,"stores",params.storeId,"orders",id),{
+  await updateDoc(doc(db, "stores", params.storeId, "orders", id), {
     ...orderData,
     id,
-    updateAt:serverTimestamp(),
+    updateAt: serverTimestamp(),
   });
 
   const session = await stripe.checkout.sessions.create({
     line_items,
-    mode:"payment",
-    billing_address_collection:"required",
-    shipping_address_collection:{
-        allowed_countries:["US","CA","GB","AU","VN"]
+    mode: "payment",
+    billing_address_collection: "required",
+    shipping_address_collection: {
+      allowed_countries: ["US", "CA", "GB", "AU", "VN"],
     },
-    phone_number_collection:{
-        enabled:true,
+    phone_number_collection: {
+      enabled: true,
     },
-    success_url:`${process.env.FRONTEND_STORE_URL}/cart?success=1`,
-    cancel_url:`${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
-    metadata:{
-        orderId:id,
-        storeId:params.storeId,
-    }
+    success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
+    cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
+    metadata: {
+      orderId: id,
+      storeId: params.storeId,
+    },
   });
 
-  return NextResponse.json({url:session.url},{headers:corsHeaders});
+  return NextResponse.json({ url: session.url }, { headers: corsHeaders });
 };
--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
-const isProtectedRoute= createRouteMatcher([
-    '/'
-])
+const isProtectedRoute = createRouteMatcher(["/"]);
 
-export default clerkMiddleware((auth,req)=>{
-    if(isProtectedRoute(req)) auth().protect();
+export default clerkMiddleware((auth, req) => {
+  if (isProtectedRoute(req)) auth().protect();
 });
-
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
